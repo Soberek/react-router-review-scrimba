adbc01fe4ee0baad8ea81774472abd6c4006ba63
Soberek <59960384+Soberek@users.noreply.github.com>
2023-04-23 10:25:45 +0200

    feat: changed Link to NavLink
    Now active navbar item is highlighted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const handleActiveLink = ({ isActive }: { isActive: boolean }) =>
+    isActive ? "active_link" : "";
+
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="currencies">Currencies</Link>
+      <NavLink to="/" className={handleActiveLink}>
+        Home
+      </NavLink>
+      <NavLink to="currencies" className={handleActiveLink}>
+        Currencies
+      </NavLink>
     </nav>
   );
 };
